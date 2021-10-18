9baaf542af392f25f7609c551fa01605b97e11d1
lucawic <lucaswick@email.arizona.edu>
2021-10-17 21:25:38 -0700

    Welcome back to the struggle bus, thought this was closer to working than it actually is lololololololololololololol :(

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,26 @@
+
+const Sequelize = require("sequelize");
 require('dotenv').config();
-const Sequelize = require('sequelize');
 
-let sequelize;
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize (process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-    port: 3001,
-  }
-);
-}
+module.exports = sequelize;
 
 /*const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-  */
+  
 
 module.exports = sequelize;
+
+*/
